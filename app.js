4ef50de32ee5d2460a1d2ea72fc3336f10165df0
Jay Jackson <jaynickjackson@gmail.com>
2018-01-29 10:50:45 -0500

    added test route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let app = express()
 
 let port = 3000
 
+app.use('/api/test', (req, res) => {
+  res.send('Server says, "Hello!"')
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
