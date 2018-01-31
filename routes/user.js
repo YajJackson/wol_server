f753d27394b8fc4436248f4497e93bf4e2d667e1
Jay Jackson <jaynickjackson@gmail.com>
2018-01-31 12:57:05 -0500

    added jwt to user signup

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,19 @@
 let router = require('express').Router()
 let sequelize = require('../db')
 let bcrypt = require('bcryptjs')
+let jwt = require('jsonwebtoken')
 let User = sequelize.import('../models/user.js')
 
-router.post('/create', (req, res) => {
+router.post('/signup', (req, res) => {
   User.create({
     username: req.body.user.username,
     passwordhash: bcrypt.hashSync(req.body.user.password, 10)
   }).then(
-    (user) => res.send({message: 'create user success', data: user}),
+    (user) => res.send({message: 'create user success', data: user, token: jwt.sign({id: user.id}, 'super_secret', {expiresIn: 60*60*24})}),
     (err) => res.send({message: 'create user failed', data: err.message})
   )
 })
+
+
 
 module.exports = router
