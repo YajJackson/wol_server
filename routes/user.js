3252970c656cb6136ad71875ecbbb69a0054e7ec
Jay Jackson <jaynickjackson@gmail.com>
2018-01-31 13:36:34 -0500

    hid jwt secret

--- routes/user.js
+++ routes/user.js
@@ -9,11 +9,26 @@ router.post('/signup', (req, res) => {
     username: req.body.user.username,
     passwordhash: bcrypt.hashSync(req.body.user.password, 10)
   }).then(
-    (user) => res.send({message: 'create user success', data: user, token: jwt.sign({id: user.id}, 'super_secret', {expiresIn: 60*60*24})}),
+    (user) => res.send({message: 'create user success', data: user, token: jwt.sign({ id: user.id }, 'super_secret', { expiresIn: 60*60*24 })}),
     (err) => res.send({message: 'create user failed', data: err.message})
   )
 })
 
-
+router.post('/signin', (req, res) => {
+  User.findOne({ where: { username: req.body.user.username } }).then(
+    (user) => {
+      if (user) {
+        bcrypt.compare(req.body.user.password, user.passwordhash, (err, matches) => {
+          matches ?
+            res.send({message: 'signin user success', data: user, token: jwt.sign({ id: user.id }, 'super_secret', { expiresIn: 60*60*24 })}) :
+            res.send({message: 'signin user failed, Incorrect password'})
+        })
+      } else {
+        res.send({message: 'signin user failed, User not found'})
+      }
+    },
+    (err) => res.send({message: 'signin user failed', data: err.message})
+  )
+})
 
 module.exports = router
